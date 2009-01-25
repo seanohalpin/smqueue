3c3e8846aaddb77576489939de1973759c71e20c
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-01-25 22:02:26 +0000

    don't bail if adapter won't load

--- lib/smqueue.rb
+++ lib/smqueue.rb
@@ -198,10 +198,18 @@ end
 
 # require adapters relative to invocation path first, then from lib
 [$0, __FILE__].each do |path|
+  p [:path, path]
   base_path = File.expand_path(File.dirname(path))
+  p [:base_path, base_path]
   adapter_path = File.join(base_path, 'smqueue', 'adapters', '*.rb')
+  p [:adapter_path, adapter_path]
   Dir[adapter_path].each do |file|
-    require file
+    p [:file, file]
+    begin
+      require file
+    rescue Object => e
+      warn "warning: could not load adapter '#{file}'. Reason: #{e}"
+    end
   end
 end
 
