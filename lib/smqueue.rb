9f5e0ebe4e1c1414f9b53e2afde998e3c37790bd
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-02-19 00:15:15 +0000

    bumped release number and added ditz bug tracking

--- lib/smqueue.rb
+++ lib/smqueue.rb
@@ -36,7 +36,7 @@ module SMQueue
 
   # Mr Bones project skeleton boilerplate
   # :stopdoc:
-  VERSION = '0.2.0'
+  VERSION = '0.3.0'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
