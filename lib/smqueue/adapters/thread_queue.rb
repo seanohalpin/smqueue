8b10695b24ad3f5af5d7bcd3897ac085ac6c9321
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-03-17 00:46:44 +0000

    added thread_queue adapter (and various tasks)

---
+++ lib/smqueue/adapters/thread_queue.rb
@@ -0,0 +1,58 @@
+require 'thread'
+
+module SMQueue
+  class ThreadQueueAdapter < Adapter
+    QUEUES = { }
+    class Configuration < AdapterConfiguration
+      def initialize(*args, &block)
+        p [self.class, args]
+        super
+      end
+      has :name, :kind => String  do
+        doc "symbolic name for queue"
+        from Symbol do |s|
+          s.to_s
+        end
+      end
+    end
+
+    class << self
+      def new(params)
+        config = params[:configuration]
+        name = config[:name]
+        if QUEUES.key?(config[:name])
+          QUEUES[name]
+        else
+          QUEUES[name] = super(params)
+        end
+      end
+    end
+
+    doc "in memory threadsafe queue"
+    has :queue do
+      doc "internal ref to thread-safe Queue"
+      init { Queue.new }
+    end
+
+    def initialize(*args, &block)
+      super
+      QUEUES[configuration.name] = self
+    end
+
+    def put(*args, &block)
+      p [:tput, args]
+      queue.enq(*args)
+    end
+
+    def get(*args, &block)
+      p [:tget]
+      loop do
+        p [:tget, :loop]
+        input = queue.deq
+        msg = SMQueue::Message.new(:body => input)
+        yield(msg)
+      end
+    end
+  end
+end
+
