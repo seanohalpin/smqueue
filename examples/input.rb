8f01bab8e2490f7b381dfcfa5937bebe38ed883a
Sean O'Halpin <seano@seano-dellmini9.(none)>
2009-11-10 16:34:26 +0000

    tweaked examples a little

--- examples/input.rb
+++ examples/input.rb
@@ -5,7 +5,12 @@ require 'pp'
 script_path = File.dirname(__FILE__)
 configuration = YAML::load(File.read(File.join(script_path, "config", "example_config.yml")))
 
-input_queue = SMQueue.new(:configuration => configuration[:input])
+if ARGV.size > 0
+  queue_name = ARGV[0].to_sym
+else
+  queue_name = :input
+end
+input_queue = SMQueue.new(:configuration => configuration[queue_name])
 
 input_queue.get do |msg|
   pp msg
--- examples/output.rb
+++ examples/output.rb
@@ -4,8 +4,15 @@ require 'smqueue'
 script_path = File.dirname(__FILE__)
 configuration = YAML::load(File.read(File.join(script_path, "config", "example_config.yml")))
 
-input_queue = SMQueue.new(:configuration => configuration[:readline])
-output_queue = SMQueue.new(:configuration => configuration[:output])
+if ARGV.size > 0
+  input_queue_name = ARGV[0].to_sym
+  output_queue_name = ARGV[1].to_sym
+else
+  input_queue_name = :readline
+  output_queue_name = :output
+end
+input_queue = SMQueue.new(:configuration => configuration[input_queue_name])
+output_queue = SMQueue.new(:configuration => configuration[output_queue_name])
 
 input_queue.get do |msg|
   output_queue.put msg.body
